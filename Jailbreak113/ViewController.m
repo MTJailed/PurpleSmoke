0d57ce890f79eaa0a0fde7d308549f2aa7e7587e
MTJailed <semvoigtlander@outlook.com>
2018-04-26 21:17:05 +0200

    Initial Commit

---
+++ Jailbreak113/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Jailbreak113
+//
+//  Created by Sem Voigtländer on 26/04/2018.
+//  Copyright © 2018 Jailed Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Jailbreak113Tests/Jailbreak113Tests.m
@@ -0,0 +1,39 @@
+//
+//  Jailbreak113Tests.m
+//  Jailbreak113Tests
+//
+//  Created by Sem Voigtländer on 26/04/2018.
+//  Copyright © 2018 Jailed Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Jailbreak113Tests : XCTestCase
+
+@end
+
+@implementation Jailbreak113Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
